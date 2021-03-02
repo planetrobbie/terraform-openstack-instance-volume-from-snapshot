4a5950d94eb9218ce7c9a70ffad4a3aac8fa66af
Sébastien Braun <sebbraun@gmail.com>
2021-03-02 14:09:50 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,56 @@
+terraform {
+required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
+# Configure the OpenStack Provider
+provider "openstack" {
+  user_name   = var.user_name
+  tenant_id   = var.tenant_id
+  password    = var.password
+  auth_url    = var.auth_url
+  region      = var.region
+}
+
+resource "openstack_blockstorage_volume_v2" "vol-01" {
+  name = var.volume_name_01
+  size = 110
+  snapshot_id = var.snapshot_id
+}
+
+#resource "openstack_blockstorage_volume_v2" "vol-02" {
+#  name = var.volume_name_02
+#  size = 1
+#}
+
+resource "openstack_compute_keypair_v2" "yet-keypair" {
+  name       = "yet-keypair"
+  public_key = var.keypair
+}
+
+resource "openstack_compute_instance_v2" "myinstance" {
+  name            = var.instance_name
+  image_id        = var.image_id
+  flavor_id       = var.flavor_id
+  key_pair        = openstack_compute_keypair_v2.yet-keypair.id
+  security_groups = ["default"]
+
+  network {
+    name = var.network
+  }
+}
+
+resource "openstack_compute_volume_attach_v2" "attached" {
+  instance_id = openstack_compute_instance_v2.myinstance.id
+  volume_id   = openstack_blockstorage_volume_v2.vol-01.id
+}
+
+#resource "openstack_compute_volume_attach_v2" "attached-01" {
+#  instance_id = var.openstack_compute_instance_v2.myinstance.id
+#  volume_id   = var.openstack_blockstorage_volume_v2.vol-02.id
+#}
---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "instance_name" {
+  description = "name of instance"
+  default = "instance-01"
+}
+
+variable "user_name" {
+  description = "OpenStack user"
+}
+
+variable "password" {
+  description = "OpenStack pwd"
+}
+
+variable "auth_url" {
+  description = "OpenStack Auth URL"
+  default = "https://auth.cloud.ovh.net/v3/"
+}
+
+variable "tenant_id" {
+  description = "OpenStack tenant id"
+}
+
+variable "region" {
+  description = "OpenStack region targeted"
+  default = "BHS5"
+}
+
+variable  "keypair" {
+  description = "OpenStack public key to inject"
+}
+
+variable  "image_id" {
+  description = "OpenStack Image ID to provision"
+}
+
+variable  "flavor_id" {
+  description = "OpenStack Flavor ID"
+  default = "d64c3ef6-c061-4e44-85a0-a7f03191b092"
+}
+
+variable "network" {
+  description = "OpenStack network name"
+  default = "Ext-Net"
+}
+
+variable "volume_name_01" {
+  description = "OpenStack volume 01 name"
+  default = "vol01"
+}
+
+variable "volume_name_02" {
+  description = "OpenStack volume 02 name"
+  default = "vol02"
+}
+
+variable "snapshot_id" {
+  description = "OpenStack Volume Snapshot ID"
+}
